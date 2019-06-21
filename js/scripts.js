11002dbdcd2355436f6349d90e43cf89f8b2e808
Bullkip <01sinister@mail.com>
2019-06-22 00:07:59 +0300

    add some files

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+    $('.slider').slick({
+       dots:true,
+       arrows:false,
+       
+    });
+     $('.slider').css('height', $(window).height() + 'px');
+});
